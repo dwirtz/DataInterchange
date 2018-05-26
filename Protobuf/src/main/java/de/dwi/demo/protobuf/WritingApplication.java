c0f389f34025facb0cd2e3748191edb81440c358
dwirtz <daniel.wi@web.de>
2018-05-26 17:03:10 +0200

    Adding proto file describing the model. Adding example for a reading and writing application.

--- Protobuf/src/main/java/de/dwi/demo/protobuf/WritingApplication.java
+++ Protobuf/src/main/java/de/dwi/demo/protobuf/WritingApplication.java
@@ -12,16 +12,22 @@ public class WritingApplication {
         stopAreaBuilder.setId("Aachen Bushof");
         stopAreaBuilder.setType(Model.StopArea.StopAreaType.A);
 
-        final Model.NetPoint.Builder netPointBuilder = Model.NetPoint.newBuilder();
-        netPointBuilder.setId("1011");
-
-        final Model.Location.Builder locationBuilder = Model.Location.newBuilder();
-        locationBuilder.setGpsX(6090225L);
-        locationBuilder.setGpsY(50777322L);
-        netPointBuilder.setLocation(locationBuilder.build());
-
-        stopAreaBuilder.addPoint(netPointBuilder.build());
+        stopAreaBuilder.addPoint(createNetPoint("1011", 6090225L, 50777322L));
+        stopAreaBuilder.addPoint(createNetPoint("1012", 6090225L, 50777322L));
+        stopAreaBuilder.addPoint(createNetPoint("1013", 6090225L, 50777322L));
+        stopAreaBuilder.addPoint(createNetPoint("1014", 6090225L, 50777322L));
 
         stopAreaBuilder.build().writeTo(new FileOutputStream("./Protobuf/src/main/resources/StopAreas"));
+    }
+
+    private static Model.NetPoint createNetPoint(final String netPointId, final long gpsX, final long gpsY) {
+        final Model.NetPoint.Builder netPointBuilder = Model.NetPoint.newBuilder();
+        netPointBuilder.setId(netPointId);
+
+        final Model.Location.Builder locationBuilder = Model.Location.newBuilder();
+        locationBuilder.setGpsX(gpsX);
+        locationBuilder.setGpsY(gpsY);
+        netPointBuilder.setLocation(locationBuilder.build());
+        return netPointBuilder.build();
     }
 }
